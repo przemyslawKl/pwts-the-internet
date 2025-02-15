307119cba000eb3cd68a88fa040a017ed00e5cb9
przemyslawKl <klobus.p@gmail.com>
2025-02-15 21:22:00 +0100

    Added new hooks for tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,24 @@
 import { expect, test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Playwright Page Basic Check', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test('has title', async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test('get started link title', async ({ page }) => {
+    await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright');
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  test('get started link', async ({ page }) => {
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
 });
